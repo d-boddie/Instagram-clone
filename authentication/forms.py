ceba4a70320f4a3022903ff007a5c8cbff4590f7
Timothy Kepple <timkep1@yahoo.com>
2021-04-07 14:05:36 -0500

    Hopefully works this time

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import InstagramUser
 
 
 class LoginForm(forms.Form):
@@ -11,3 +12,17 @@ class SignupForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput)
     first_name = forms.CharField(max_length=150)
     last_name = forms.CharField(max_length=150)
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = InstagramUser
+        fields = [
+            "bio",
+            "website",
+            "first_name",
+            "last_name"
+        ]
+
+class EditAccountForm(forms.Form):
+    username = forms.CharField(max_length=40)
+    password = forms.CharField(widget=forms.PasswordInput) 
