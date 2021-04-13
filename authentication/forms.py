e4adc6c9acae17c719b1d9cc65ef6c89a324204c
Cesar Ramos <cesaramos1452@yahoo.com>
2021-04-12 19:48:55 -0700

    Finished styling the signup page!

--- authentication/forms.py
+++ authentication/forms.py
@@ -14,11 +14,29 @@ class LoginForm(forms.Form):
         widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
     
 class SignupForm(forms.Form):
-    username = forms.CharField(max_length=40)
-    password = forms.CharField(widget=forms.PasswordInput)
-    email = forms.EmailField(widget=forms.EmailInput)
-    first_name = forms.CharField(max_length=150)
-    last_name = forms.CharField(max_length=150)
+    first_name = forms.CharField(
+        max_length=50,
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    
+    last_name = forms.CharField(
+        max_length=50,
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    
+    email = forms.EmailField(
+        label='',
+        widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+   
+    username = forms.CharField(
+        max_length=50,
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+
+    password = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
 class EditProfileForm(forms.ModelForm):
     class Meta:
