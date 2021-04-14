86b23bff46c7856ee29bfbbe9f45013bc0658812
kyle <kyle.hill56@yahoo.com>
2021-04-13 22:26:18 -0400

    added photos to homepage

--- photo/models.py
+++ photo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from authentication.models import InstagramUser
+from comment.models import Comment
 
 
 # Create your models here.
@@ -13,6 +14,9 @@ class Photo(models.Model):
     button = models.CharField(max_length=50, default="Like")
     poster = models.ForeignKey(
         InstagramUser, related_name="poster", on_delete=models.CASCADE, default=True)
+
+    comment = models.ForeignKey(
+        Comment, related_name="comment", on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return self.caption
