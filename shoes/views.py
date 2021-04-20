f1ebf63d014f0f713a4135f77d01746a1d3e4990
Deidre Boddie <deidre.boddie@gmail.com>
2021-04-19 22:38:55 -0700

    updated shoe api to version 2

--- shoes/views.py
+++ shoes/views.py
@@ -3,7 +3,7 @@ import requests
 
 # Create your views here.
 
-URL = ('https://api.thesneakerdatabase.com/v1/sneakers?limit=98&brand=nike')
+URL = ('https://api.thesneakerdatabase.dev/v2/sneakers?limit=100&brand=nike')
 
 def shoes(request):
     try:
@@ -11,8 +11,8 @@ def shoes(request):
         shoe_image = requests.get(URL).json()
         images = shoe_image["results"]
         for shoe in images:
-            if shoe["media"]["smallImageUrl"]:
-                shoe_pic.append(shoe["media"]["smallImageUrl"])
+            if shoe["image"]["small"]:
+                shoe_pic.append(shoe["image"]["small"])
         return render(request, "shoes.html", {"shoe_pic": shoe_pic })
     except ConnectionError:
         return render(request, '500.html')
